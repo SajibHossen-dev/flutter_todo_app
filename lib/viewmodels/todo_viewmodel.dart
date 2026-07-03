c3d2f25018f764b54877fc06c90cc409c71a86e5
sajib Hossen <sajibhossen560@gmail.com>
2026-07-03 14:41:48 +0600

    create add todo function

---
+++ lib/viewmodels/todo_viewmodel.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_app/models/todo_model.dart';
+
+class TodoViewmodel extends ChangeNotifier {
+  final List<TodoModel> _todos = [];
+  List<TodoModel> get todos => _todos;
+
+  void addTodo(String title) {
+    _todos.add(
+      TodoModel(title: title , isCompleted: false),
+      
+    );
+    notifyListeners();
+  }
+}
