721336113d1f3769495596f4d7098331c4708950
sajib Hossen <sajibhossen560@gmail.com>
2026-07-04 08:46:45 +0600

    add stf widget and create add todo btn and show todo frontend

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,26 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_app/viewmodels/todo_viewmodel.dart';
+import 'package:flutter_todo_app/widgets/todo_title.dart';
+import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Todo App")
-    ),
+  State<HomePage> createState() => _HomePageState();
+}
 
-    body: Padding(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        children: [
-          TextField(
-            decoration: const InputDecoration(
-              hintText: "Enter Task",
-              border: OutlineInputBorder()
+class _HomePageState extends State<HomePage> {
+  final TextEditingController controller = TextEditingController();
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final vm = context.watch<TodoViewmodel>();
+    return Scaffold(
+      appBar: AppBar(title: Text("Todo App")),
+
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                hintText: "Enter Task",
+                border: OutlineInputBorder(),
+              ),
             ),
-          )
-        ],
-      )
-    ),
+            const SizedBox(height: 15),
+
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  context.read<TodoViewmodel>().addTodo(controller.text);
+                  controller.clear();
+                },
+                child: const Text("Add Task"),
+              ),
+            ),
+            const SizedBox(height: 15,),
+            Expanded(
+              child: ListView.builder(
+                itemCount: vm.todos.length,
+                itemBuilder : (context , index){
+                  return TodoTitle(todo: vm.todos[index]);
+                } 
+                )
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
